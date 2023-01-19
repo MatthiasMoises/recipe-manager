f9a136dd52c12a1d68189ccf076e4a411fd1230d
Matthias Moises <mail@matthias-moises.de>
2023-01-19 17:49:56 +0100

    Added more unit tests

--- server/tests/IngredientEndpointsTest.php
+++ server/tests/IngredientEndpointsTest.php
@@ -7,15 +7,57 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class IngredientEndpointsTest extends TestCase
 {
-    public function test_that_names_endpoint_returns_a_successful_response()
-    {
+    use DatabaseTransactions;
+
+    private $token = 'xCKc90BKy2K2xRJc';
+
+    public function test_that_names_endpoint_returns_a_successful_response() {
         $response = $this->call('GET', '/ingredient/names');
 
         $this->assertEquals(200, $response->status());
     }
 
-    public function test_that_seeded_ingredient_data_is_available_in_database()
-    {
+    public function test_that_show_ingredients_endpoint_returns_a_successful_response() {
+        $response = $this->call('GET', '/ingredient');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function test_making_an_api_request_to_save_ingredient_endpoint() {
+        $response = $this->post( '/ingredient', [
+            'name' => 'OMmqaJarvP',
+            'cup_in_grams' => 5,
+            'level_tablespoon_in_grams' => 5,
+            'heaped_tablespoon_in_grams' => 5,
+            'level_teaspoon_in_grams' => 5
+        ], ['App-Token' => $this->token]);
+
+        $response->seeJsonContains([
+            'name' => 'OMmqaJarvP'
+        ]);
+    }
+
+    public function test_making_an_api_request_to_update_ingredient_endpoint() {
+        $response = $this->put( '/ingredient/1', [
+            'name' => 'YHERrQALFO',
+            'cup_in_grams' => 10,
+            'level_tablespoon_in_grams' => 10,
+            'heaped_tablespoon_in_grams' => 10,
+            'level_teaspoon_in_grams' => 10
+        ], ['App-Token' => $this->token]);
+
+        $response->seeJsonContains([
+            'name' => 'YHERrQALFO',
+        ]);
+    }
+
+    public function test_making_an_api_request_to_delete_ingredient_endpoint() {
+        $response = $this->call('DELETE', '/ingredient/1');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function test_that_seeded_ingredient_data_is_available_in_database() {
         $this->seeInDatabase('ingredients', ['name' => 'Milch']);
     }
 }
--- server/tests/RecipeEndpointsTest.php
+++ server/tests/RecipeEndpointsTest.php
@@ -7,6 +7,10 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class RecipeEndpointsTest extends TestCase
 {
+    use DatabaseTransactions;
+
+    private $token = 'xCKc90BKy2K2xRJc';
+
     public function test_that_base_recipes_endpoint_returns_a_successful_response()
     {
         $response = $this->call('GET', '/recipe');
@@ -14,10 +18,72 @@ class RecipeEndpointsTest extends TestCase
         $this->assertEquals(200, $response->status());
     }
 
+    /*
+    public function test_making_an_api_request_to_save_recipe_endpoint() {
+        $response = $this->post( '/recipe', [
+            'name' => 'Nudelsalat',
+            'short_description' => 'Lecker Nudelsalat',
+            'number_of_people' => 2,
+            'ingredients' => $this->prepareIngredients(),
+            'preparation_steps' => $this->preparePreparationSteps()
+        ], ['App-Token' => $this->token]);
+
+        $response->seeJsonContains([
+            'name' => 'Nudelsalat'
+        ]);
+    }
+    */
+
+    /*
+    public function test_making_an_api_request_to_update_recipe_endpoint() {
+        $response = $this->patch( '/recipe/1', [
+            'name' => 'Nudelsalat',
+            'short_description' => 'Lecker Nudelsalat, improved',
+            'number_of_people' => 2,
+            'ingredients' => $this->prepareIngredients(),
+            'preparation_steps' => $this->preparePreparationSteps()
+        ], ['App-Token' => $this->token]);
+
+        $response->seeJsonContains([
+            'short_description' => 'Lecker Nudelsalat, improved',
+        ]);
+    }
+    */
+
     public function test_that_single_recipe_endpoint_returns_a_successful_response()
     {
         $response = $this->call('GET', '/recipe/1');
 
         $this->assertEquals(200, $response->status());
+    }
+
+    public function test_making_an_api_request_to_delete_recipe_endpoint() {
+        $response = $this->call('DELETE', '/recipe/1');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    private function prepareIngredients() {
+        $ingredients = array(
+            array(
+                "name" => "Schweinefleisch",
+                "unit" => "kg",
+                "amount" => "1"
+            )
+        );
+
+        return json_encode($ingredients, JSON_UNESCAPED_SLASHES);
+    }
+
+    private function preparePreparationSteps () {
+        $preparationSteps = array(
+            array(
+                "short_description" => "Klopfen",
+                "preparation_time" => "5",
+                "cooking_time" => "5"
+            )
+        );
+
+        return json_encode($preparationSteps, JSON_UNESCAPED_SLASHES);
     }
 }
